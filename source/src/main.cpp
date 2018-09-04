310b5f3778338a1ca3b26ef633714408abe3966a
Vyom Fadia <vyom.fadia@outlook.com>
2018-09-04 23:55:35 +0100

    Make things work! And start load in the window

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,7 +1,26 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+#include <viking/SDLWindow.hpp>
+#include <viking/IRenderer.hpp>
+#include <viking/IUniformBuffer.hpp>
+#include <viking/IComputePipeline.hpp>
+#include <viking/IComputeProgram.hpp>
+
 #include <iostream>
+
+using namespace viking;
+
+IRenderer* renderer;
+IModelPool* model_pool;
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello World" << std::endl;
+	const RenderingAPI renderingAPI = RenderingAPI::GL3;
+	const WindowingAPI windowAPI = WindowingAPI::SDL;
+
+	IWindow* window = IWindow::createWindow(WindowDescriptor("Sandblox Client", 1280, 720), windowAPI, renderingAPI);
+
 	return 0;
 }
